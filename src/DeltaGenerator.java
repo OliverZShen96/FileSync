6f5844f36be111e4d4d4a3ce9fb122a5d6926880
Oliver Shen <olivershen96@gmail.com>
2016-11-27 20:33:36 +1100

    Minor changes to findCommonSubstrings method to include the indexes of the substrings from both strings

--- src/DeltaGenerator.java
+++ src/DeltaGenerator.java
@@ -11,10 +11,22 @@ public class DeltaGenerator {
 		byte[] a1 = {'a','b','c', 'd', 'e', 'd', 'a', 'a', 'd', 'a', 'a'};
 		byte[] a2 = {'a','d','b','c', 'd', 'a', 'a', 'd', 'a', 'a'};
 		ArrayList<Integer> commonStringLengths = new ArrayList<Integer>();
-		ArrayList<Integer> commonStringIndexes = new ArrayList<Integer>();
-		diff(a1,a2, commonStringLengths, commonStringIndexes, 0, a1.length, 0, a2.length, 1);
+		ArrayList<Integer> commonStringIndexesOriginal = new ArrayList<Integer>();
+		ArrayList<Integer> commonStringIndexesEdited = new ArrayList<Integer>();
+		findCommonSubstrings(a1,a2, commonStringLengths, commonStringIndexesOriginal, commonStringIndexesEdited, 0, a1.length, 0, a2.length, 1);
 		
-		for (Integer i : commonStringIndexes) {
+		System.out.println("Common substrings are at the following indexes in the original sequence:");
+		for (Integer i : commonStringIndexesOriginal) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Common substrings are at the following indexes in the edited sequence:");
+		for (Integer i : commonStringIndexesEdited) {
+			System.out.println(i);
+		}
+		
+		System.out.println("The common substrings are of these lengths");
+		for (Integer i : commonStringLengths) {
 			System.out.println(i);
 		}
 	}
@@ -57,20 +69,20 @@ public class DeltaGenerator {
 	// Finds all common substrings in two byte arrays larger than a certain size
 	// Places matches in the commonStringLengths and commonStringIndexes arraylists
 	// Works recursively
-	public static void diff(byte[] original, byte[] edited, ArrayList<Integer> commonStringLengths, ArrayList<Integer> commonStringIndexes, int startOriginal, int endOriginal, int startEdited, int endEdited, int minSubstringSize) {
-		System.out.println("diffing");
+	public static void findCommonSubstrings(byte[] original, byte[] edited, ArrayList<Integer> stringLengths, ArrayList<Integer> stringIndexesOriginal, ArrayList<Integer> stringIndexesEdited, int startOriginal, int endOriginal, int startEdited, int endEdited, int minSubstringSize) {
 		int[] LCS = longestCommonSubstring(original, edited, startOriginal, endOriginal, startEdited, endEdited, minSubstringSize);
 		
 		// stop searching if no long common substrings are found
 		if (LCS == null) return;
 		
 		// add LCS is found
-		commonStringLengths.add(LCS[0]);
-		commonStringIndexes.add(LCS[1]);
+		stringLengths.add(LCS[0]);
+		stringIndexesOriginal.add(LCS[1]);
+		stringIndexesEdited.add(LCS[2]);
 		
 		// search regions before and after
-		diff(original, edited, commonStringLengths, commonStringIndexes, startOriginal, LCS[1], startEdited, LCS[2], minSubstringSize);
-		diff(original, edited, commonStringLengths, commonStringIndexes, LCS[0]+LCS[1], endOriginal, LCS[0]+LCS[2], endEdited, minSubstringSize);
+		findCommonSubstrings(original, edited, stringLengths, stringIndexesOriginal, stringIndexesEdited, startOriginal, LCS[1], startEdited, LCS[2], minSubstringSize);
+		findCommonSubstrings(original, edited, stringLengths, stringIndexesOriginal, stringIndexesEdited, LCS[0]+LCS[1], endOriginal, LCS[0]+LCS[2], endEdited, minSubstringSize);
 	}
 	
 	// Given a file and an edit of it, create a file that can be used in conjunction with the original to recreate the edit
