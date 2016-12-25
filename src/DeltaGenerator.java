313bd366346a6583f258ebc538fc33660c534d47
OliverZShen96 <oliver.z.shen96@gmail.com>
2016-12-25 21:10:07 +1100

    Started work on delta generator function Fixed issue with LCS finder where it would count outside of specified domain Changed arguments of LCS finder to use last array index instead of array length

--- src/DeltaGenerator.java
+++ src/DeltaGenerator.java
@@ -1,11 +1,4 @@
-import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Scanner;
-import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class DeltaGenerator {
@@ -19,14 +12,14 @@ public class DeltaGenerator {
 	}
 
 	// Find the longest common substring between two byte arrays
-	private static int[] longestCommonSubstring(byte[] s1, byte[] s2, int s1Index1, int s1Index2, int s2Index1, int s2Index2, int minSubstringSize) {
+	private static int[] longestCommonSubstring(byte[] s1, byte[] s2, int s1Start, int s1End, int s2Start, int s2End, int minSubstringSize) {
 		int length = 0;
 		int index1 = 0;
 		int index2 = 0;
 		
 		// iterate through the two byte arrays
-		for (int i = s1Index1; i < s1Index2; i++) {
-			for (int j = s2Index1; j < s2Index2; j++) {
+		for (int i = s1Start; i <= s1End; i++) {
+			for (int j = s2Start; j <= s2End; j++) {
 				
 				// comparing bytes one by one
 				if (s1[i] != s2[j]) continue;
@@ -41,6 +34,7 @@ public class DeltaGenerator {
 					a++;
 					b++;
 					if (a >= s1.length || b >= s2.length) break;
+					if (a > s1End || b > s2End) break;
 				}
 				
 				// compare with LCS
@@ -78,7 +72,7 @@ public class DeltaGenerator {
 		count++;
 		
 		// search regions before and after
-		findCommonSubstrings(original, edited, stringLengths, editIndexes, startOriginal, LCS[1], startEdited, LCS[2], minSubstringSize);
+		findCommonSubstrings(original, edited, stringLengths, editIndexes, startOriginal, LCS[1]-1, startEdited, LCS[2]-1, minSubstringSize);
 		findCommonSubstrings(original, edited, stringLengths, editIndexes, LCS[0]+LCS[1], endOriginal, LCS[0]+LCS[2], endEdited, minSubstringSize);
 	}
 	
@@ -87,22 +81,43 @@ public class DeltaGenerator {
 		TreeMap<Integer, Integer> stringLengths = new TreeMap<Integer, Integer>();
 		TreeMap<Integer, Integer> editIndexes = new TreeMap<Integer, Integer>();
 		
-		findCommonSubstrings(a1,a2, stringLengths, editIndexes, 0, a1.length, 0, a2.length, 1);
+		findCommonSubstrings(a1,a2, stringLengths, editIndexes, 0, a1.length-1, 0, a2.length-1, 1);
 		
 		// iterate through indexes of common strings
+		
+		// key i is the index in the original data
+		// value from stringLengths is length of common string
+		// value from editIndexes is the index in the edited data
+		
+		/*
+		 * int counter = 0;
 		for (int i : stringLengths.keySet()) {
 			System.out.print("index a " + i + "|");
 			System.out.print("length " + stringLengths.get(i) + "|");
 			System.out.println("index b " + editIndexes.get(i));
 			
+			// common string starts at current place
 			
-			/*
-			 *  TODO:
-			 *  
-			 *  - Iterating through the common strings, compare the two pieces of data with one another
-			 *  - Need to handle copy/remove/insert operations based on common string locations
-			 *  
-			 */
+			int diff = i - editIndexes.get(i);
+			if (diff == 0) {
+				// Common strings have the same indexes
+				// Copy operation
+				System.out.println("Copy " + stringLengths.get(i));
+			} else if (diff > 0){
+				// common string is later in the original string
+				// Remove then copy
+				System.out.println("Remove " + diff);
+				System.out.println("Copy " + stringLengths.get(i));
+			} else {
+				// Common string is earlier in the original string
+				// Write then copy
+				System.out.print("Write ");
+				for (int j = diff; j < 0; j++) {
+					System.out.print(Character.toChars(a2[i-j]));
+				}
+				System.out.println();
+			}
 		}
+		*/
 	}
 }
